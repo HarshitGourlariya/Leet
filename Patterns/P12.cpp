d6d102b03fabb952a7e73c8129086d44ba6f94a4
HarshitGourlariya <hgourlariya@gmail.com>
2026-02-21 21:37:23 +0530

    Pyramid Symmetry Day-5

---
+++ Patterns/P12.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+     int i,n,j;
+     cin >> n;
+     for(i=0;i<n;i++)
+ {
+    for(j=0;j<n-i-1;j++)
+    {
+        cout << " ";
+    }
+    for(j=0;j<2*i+1;j++)
+    {
+        cout << "*";
+    }
+    for(j=0;j<n-i-1;j++)
+    {
+        cout << " ";
+    }
+    cout<< endl;
+ }
+ for(i=0;i<n;i++)
+    {
+         for(j=0;j<i;j++)
+        {
+            cout << " ";
+        }
+        for(j=0;j<2*n-(2*i+1);j++)
+        {
+            cout << "*";
+        }
+        for(j=0;j<i;j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
