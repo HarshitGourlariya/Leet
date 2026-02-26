b079f22fdfaa7416c7d52c42d4338528bcd9d9e0
HarshitGourlariya <hgourlariya@gmail.com>
2026-02-26 09:05:35 +0530

    Bank Gltich

---
+++ Contests/Contest-2/BankGlitch.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	
+	int T,A,B,X,Y,i;
+	int trade,profit,Total,final;
+	cin >> T;
+	for(i=0;i<T;i++)
+	{
+	    
+	    cin >> A >> B >> X >> Y;
+	    trade = A/X;
+	    profit=Y-X;
+	    Total=trade*profit;
+	    final = Total+A+B;
+	    cout << final << endl;
+	    }
+}
