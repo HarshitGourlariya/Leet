870cf569208358ccd3633f0f5df7916bc62defdc
HarshitGourlariya <hgourlariya@gmail.com>
2026-02-27 22:21:36 +0530

    Map with String

---
+++ Map/strmap.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ string s;
+ cin >>s;
+
+ map<int,int> mpp;
+
+ for(int i=0;i<s.size();i++)
+ {
+    mpp[s[i]]++;
+ }
+
+ int q;
+ cin >> q;
+ 
+ while(q--)
+ {
+    char p;
+    cin >> p;
+    cout << mpp[p] << endl;
+ }
+}
