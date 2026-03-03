017dac5ba2e566464bba217f1d5b56f235caa113
HarshitGourlariya <hgourlariya@gmail.com>
2026-03-03 12:20:01 +0530

    Bubble sort

--- Sorting/Bubble.cpp
+++ Sorting/Bubble.cpp
@@ -4,3 +4,10 @@ int main()
 {
 
 }
+/*
+Buuble sort mai basically right hand side pai hamesha bigger element hona chahiye and left hand side mai smaller element hona 
+chahiye 
+it takes comparision (n-1)
+time complexity hoti hai (n^2).
+Best case mai 0(n),
+*/
