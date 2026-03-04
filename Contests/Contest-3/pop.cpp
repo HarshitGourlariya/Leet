1b645834727d41cd3d2046e7e4d2f996e85e8ae3
HarshitGourlariya <hgourlariya@gmail.com>
2026-03-04 20:49:39 +0530

    Wednesday contest

---
+++ Contests/Contest-3/pop.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin >> a >> b;
+	if(a>b)
+	cout << "Alice";
+	else
+	if(b>a)
+	cout << "Bob";
+	else
+	cout << "Draw";
+
+}
