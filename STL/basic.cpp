ce276aeac5f4cfa3c89b4fd0bcb6c047199f4722
HarshitGourlariya <hgourlariya@gmail.com>
2026-03-23 18:11:45 +0530

    Sets in STL

---
+++ STL/basic.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// SETS
+int main()
+{
+    set<int> s; //aise define krte hai ek set ko 
+    set<int> s ={1,2,3,4}; // aise store kr di hai values 
+    s.insert(5); // aise insert kr skte hai values in set
+    s.erase(3); //ye use hota hai element delete krne ke liye
+
+    //ACESS ELEMENT IN THE SET
+    /*for(int val : s) */
+    // aise print karwate hai set ko 
+}
