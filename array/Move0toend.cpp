8d4ebd52767396fd817a31fb22cf216c939c0efa
HarshitGourlariya <hgourlariya@gmail.com>
2026-03-24 21:43:14 +0530

    Move Zero to end of the array

---
+++ array/Move0toend.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int i = 0; 
+
+    for(int j = 0; j < n; j++)
+    {
+        if(arr[j] != 0)
+        {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+
+    for(int k = 0; k < n; k++)
+    {
+        cout << arr[k] << " ";
+    }
+}
