40f0aa20374767283196b163a2a174332b41374e
HarshitGourlariya <hgourlariya@gmail.com>
2026-03-27 09:53:30 +0530

    Find the number that appears once , and the other number twice

---
+++ array/Findsingleindouble.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cin >> n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+
+    int hash[n+1] ={0};
+
+    for(i=0;i<n;i++)
+    {
+        hash[arr[i]]+=1;
+    }
+    for(i=1;i<n+1;i++)
+    {
+        if(hash[i]==1)
+        cout << i;
+        
+    }
+}
