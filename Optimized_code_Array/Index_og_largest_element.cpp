99dda76d9b9eca57f3705a66faafd37c9b8bcfc1
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-09 12:34:37 +0530

    Index of largest element

---
+++ Optimized_code_Array/Index_og_largest_element.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i=0,n,max=0;
+    cin >> n ;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>arr[max])
+        max = i;
+    }
+    cout << max;
+}
