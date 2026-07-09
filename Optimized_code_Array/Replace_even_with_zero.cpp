78682a5603722d8d6498bbbd4733ae49e9bbc6f1
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-09 12:19:41 +0530

    Replace even with zero

---
+++ Optimized_code_Array/Replace_even_with_zero.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i=0,n,max=0,smax;
+    cin >> n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            arr[i]=0;
+            
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        cout << arr[i]<<" ";
+    }
+    
+}
