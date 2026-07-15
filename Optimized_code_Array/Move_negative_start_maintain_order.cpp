64222c79ac0536568f87611f5bd1f2941c5d2d19
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-15 13:10:20 +0530

    Move negative numbers to start maintain order

---
+++ Optimized_code_Array/Move_negative_start_maintain_order.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin >> n;
+    int arr[n],brr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    int ind =0;
+    for(int j=0;j<n;j++)
+    {
+        if(arr[j]<0)
+        {
+            brr[ind] = arr[j];
+            ind++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {    
+        if(arr[i]>0)
+        {
+            brr[ind] = arr[i];
+            ind++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        cout << brr[i]<<" ";
+    }
+}
