b35394bf3e394569f3dc21ec845e00f15834cbe7
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-17 10:10:56 +0530

    Move even to end maintain order

---
+++ Optimized_code_Array/Move_even_end_maintain_order.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin >> n;
+    int arr[n],brr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    int j = 0;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+           brr[j]=arr[i];
+           j++;
+           }
+
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            brr[j] = arr[i];
+            j++;
+            
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        cout << brr[i]<<" ";
+    }
+}
