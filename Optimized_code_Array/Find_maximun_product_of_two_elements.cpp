f40fdde89b57c13be06bce44a2748a5175c1282c
HarshitGourlariya <hgourlariya@gmail.com>
2026-07-22 15:43:31 +0530

    Find maximum product of the two elements in the array

---
+++ Optimized_code_Array/Find_maximun_product_of_two_elements.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int max_1(int x ,int y)
+{
+    if(x>y)
+    return x;
+    else
+    return y;
+}
+
+int product(int arr[],int n)
+{
+    int i;
+    int max = INT_MIN, smax = INT_MIN;
+    int min = INT_MAX, smin = INT_MAX;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            smax = max;
+            max = arr[i];
+        }
+        else if(arr[i]>smax)
+        {
+            smax = arr[i];
+        }
+
+        if(arr[i]<min)
+        {
+            smin = min;
+            min = arr[i];
+        }
+        else if(smin>arr[i])
+        smin = arr[i];
+    }
+    return max_1(max*smax,min*smin);
+}
+
+int main()
+{
+    int i,n;
+    cin >> n;
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        cin >> arr[i];
+
+    }
+    int res = product(arr,n);
+    cout << res;
+
+}
