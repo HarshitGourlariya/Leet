da6b231029a9e4c2fd6b8c8e420bf85e8877b23e
HarshitGourlariya <hgourlariya@gmail.com>
2026-08-11 10:48:03 +0530

    query to print city names ending with vowel by hackerank

---
+++ SQL_Hacker_rank/q12.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e'
+OR CITY LIKE '%i' OR CITY LIKE '%o' OR CITY LIKE '%u';
+/*
+In this we need to end the city name with the vowel so we used the % side before the keyword
+*/
